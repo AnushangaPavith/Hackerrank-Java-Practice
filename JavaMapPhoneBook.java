ee0190809012aefaf6eabd06553a4d803fd8f6a8
anushanga pavith <anushangapavith2@gmail.com>
2023-02-15 19:11:15 +0530

    Updated two files

--- JavaMapPhoneBook.java
+++ JavaMapPhoneBook.java
@@ -1,6 +1,5 @@
 //Complete this code or write your own from scratch
 import java.util.*;
-import java.io.*;
 
 class Solution{
 	public static void findPhoneNumber()
@@ -9,6 +8,7 @@ class Solution{
 		int n=scanner.nextInt();
 		scanner.nextLine();
         Map <String, Integer> phoneBook = new HashMap<String, Integer>();
+		scanner.close();
         
 		for(int i=0;i<n;i++)
 		{
